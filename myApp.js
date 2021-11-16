4787c0cebbe9fd6751910032092353fd73116ac9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-11-16 13:00:29 -0400

    Build a simple logger

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 console.log("Hello World");
 
@@ -10,13 +11,23 @@ console.log("Hello World");
 publicPath = __dirname + "/public"
 app.use("/public", express.static(publicPath));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 indexPath = __dirname + "/views/index.html"
 app.get("/", (req, res) => {
   res.sendFile(indexPath);
 });
 
 app.get("/json", (req, res) => {
-  res.json({ "message": "Hello json" });
+  message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ "message": message.toUpperCase() });
+  } else {
+    res.json({ "message": message });
+  }
 });
 
 
