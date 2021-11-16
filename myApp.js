20547130cae66e4fe99624a6bb1ea4a46b8be4e7
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-11-16 13:24:04 -0400

    Chain a middleware function and the final handler in the app.get("/now") route

--- myApp.js
+++ myApp.js
@@ -30,6 +30,14 @@ app.get("/json", (req, res) => {
   }
 });
 
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+},
+  (req, res) => {
+    res.json({ time: req.time });
+  })
+
 
 
 
