ef1d8b681e4d034f2b9bd4fbb937d8243c2ac119
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-11-16 12:00:40 -0400

    Send the /views/index.html file as a response to GET requests to the / path

--- myApp.js
+++ myApp.js
@@ -3,10 +3,14 @@ var app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-})
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// })
 
+path = __dirname + "/views/index.html"
+app.get("/", (req, res) => {
+  res.sendFile(path);
+})
 
 
 
